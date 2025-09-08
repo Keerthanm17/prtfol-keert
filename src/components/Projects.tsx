64bb26f951793db1ec9cf1692b1342a52019c951
Keerthanm17 <keerthanmgowda3@gmail.com>
2025-09-09 00:06:37 +0530

    2nd commit

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -18,7 +18,7 @@ const Projects = () => {
       ],
       icon: Bot,
       gradient: "from-primary to-primary-glow",
-      link: "https://smart-heritage.netlify.app/",
+      link: "https://heritage-guide.netlify.app/",
       github: "https://github.com/Keerthanm17/heritage-guide"
     },
     {
